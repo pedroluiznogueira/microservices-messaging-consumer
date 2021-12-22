7313c3e35d6dc3c8d92078c8c120c71e2e43c3d7
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-22 20:10:40 -0300

    Without Configuration annotation, nothing gets created

--- src/main/java/com/github/pedroluiznogueira/microservices/messagingconsumer/configuration/RabbitMQQueueConfig.java
+++ src/main/java/com/github/pedroluiznogueira/microservices/messagingconsumer/configuration/RabbitMQQueueConfig.java
@@ -1,12 +1,27 @@
 package com.github.pedroluiznogueira.microservices.messagingconsumer.configuration;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitMQQueueConfig {
 
+    // simple way of creating queues
     @Bean
     Queue firstQueue() {
         return new Queue("FirstQueue", false);
     }
+
+    // creating queue with queue builder
+    @Bean
+    Queue secondQueue() {
+        return QueueBuilder
+                .durable("SecondQueue")
+                .autoDelete()
+                .exclusive()
+                .build();
+    }
+
 }
