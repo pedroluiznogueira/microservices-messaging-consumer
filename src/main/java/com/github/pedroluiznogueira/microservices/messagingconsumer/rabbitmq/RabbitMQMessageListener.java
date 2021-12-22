c6d659a5a631589fe408f64dd6a6c7cf74f4e0cf
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-22 16:26:34 -0300

    Initial commit

---
+++ src/main/java/com/github/pedroluiznogueira/microservices/messagingconsumer/rabbitmq/RabbitMQMessageListener.java
@@ -0,0 +1,8 @@
+package com.github.pedroluiznogueira.microservices.messagingconsumer.rabbitmq;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+
+public class RabbitMQMessageListener implements MessageListener {
+
+}
