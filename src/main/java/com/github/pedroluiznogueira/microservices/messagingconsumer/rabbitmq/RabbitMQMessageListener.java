21c9b392fdd0dc1b7fb3a19e36af9cd433b0cd83
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-22 16:29:43 -0300

    The message comes as binary object, so we parse it to a String, but the message has body and header, so we parse only the body

--- src/main/java/com/github/pedroluiznogueira/microservices/messagingconsumer/rabbitmq/RabbitMQMessageListener.java
+++ src/main/java/com/github/pedroluiznogueira/microservices/messagingconsumer/rabbitmq/RabbitMQMessageListener.java
@@ -7,6 +7,6 @@ public class RabbitMQMessageListener implements MessageListener {
 
     @Override
     public void onMessage(Message message) {
-
+        System.out.println("message = [" + message.getBody().toString() + "]");
     }
 }
