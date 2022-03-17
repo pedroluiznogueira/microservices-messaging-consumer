2fc39e89d5749de6ba1386b611faf9f71a685ebd
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-03-17 01:44:50 -0300

    article updates

--- src/main/java/com/github/pedroluiznogueira/microservices/messagingconsumer/domain/User.java
+++ src/main/java/com/github/pedroluiznogueira/microservices/messagingconsumer/domain/User.java
@@ -2,47 +2,26 @@ package com.github.pedroluiznogueira.microservices.messagingconsumer.domain;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
-@Component
-@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id", scope = User.class)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class User implements Serializable {
-
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String userId;
     private String userName;
-
-    public User(String userId, String userName) {
-        this.userId = userId;
-        this.userName = userName;
-    }
-
-    public User() {
-
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "userId='" + userId + '\'' +
-                ", userName='" + userName + '\'' +
-                '}';
-    }
 }
